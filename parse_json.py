a5e65b3440d0f1fb1f296d2e48c4f00121a0534d
zhengqunkoo <zhengqun.koo@gmail.com>
2017-05-15 11:40:10 +0800

    used ijson to load json iteratively, added gensim files

--- parse_json.py
+++ parse_json.py
@@ -1,36 +1,44 @@
-import json
+import ijson
 from pprint import pprint
 
-def values_from_datum(datum, keymatch):
-	'''
-	:para datum: list of dictionaries, each dictionary has 'type' and 'value' keys
-	:para keymatch: string to match value corresponding to 'type' key in datum
-	
-	outputs tuple of concatenated values from each keymatch, in order
-	'''
-	values = []
-	for entity in datum:
-		if entity['type'] == keymatch:
-			values.append(entity['value'])
-	return tuple(values)
+class ParseJSON(object):
+	def __init__(self, filename):
+		self.filename = filename
 
-def parse_json(filename):
-	'''
-	:para filename: .json filename
+	def get_filename(self):
+		return self.filename
 
-	outputs dictionary, where key is tuple of shortened letter entities,
-	and value is tuple of full word entities
-	'''
-	with open(filename) as f:
-		shortened_to_full = {}
-		data = json.load(f)
+	def values_from_datum(self, datum, keymatch):
+		'''
+		:para datum: list of dictionaries, each dictionary has 'type' and 'value' keys
+		:para keymatch: string to match value corresponding to 'type' key in datum
+		
+		outputs tuple of concatenated values from each keymatch, in order
+		'''
+		values = []
+		for entity in datum:
+			if entity['type'] == keymatch:
+				values.append(entity['value'])
+		return tuple(values)
 
-		for datum in data:
-			full = values_from_datum(datum['entitiesFull'], 'word')
-			shortened = values_from_datum(datum['entitiesShortened'], 'letter')
-			shortened_to_full[shortened] = full
+	def parse_json(self):
+		'''
+		:para filename: .json filename
 
-		return shortened_to_full
+		outputs dictionary, where key is tuple of shortened letter entities,
+		and value is tuple of full word entities
+		'''
+		filename = self.get_filename()
+		with open(filename) as f:
+			shortened_to_full = {}
+			for datum in ijson.items(f, 'item'):
+				full = self.values_from_datum(datum['entitiesFull'], 'word')
+				shortened = self.values_from_datum(datum['entitiesShortened'], 'letter')
+				shortened_to_full[shortened] = full
 
-for filename in ['example_training_data.json']:
-	pprint(parse_json(filename))
+			return shortened_to_full
+
+if __name__ == '__main__':
+	for filename in ['example_training_data.json']:
+		pj = ParseJSON(filename)
+		pprint(pj.parse_json())
