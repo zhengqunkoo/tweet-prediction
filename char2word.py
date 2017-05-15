1aa656ebe7a12d9943aadbb80b6a72f8975a8425
Arjo Chakravarty <arjo129@gmail.com>
2017-05-15 09:52:18 +0800

    Added basic keras model...

--- char2word.py
+++ char2word.py
@@ -1,3 +1,6 @@
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+from keras.layers.wrappers import Bidirectional
 def char2words(char_sequence,filename):
 	"""
 	A quick and dirty example of how to enumerate all 
@@ -9,4 +12,32 @@ def char2words(char_sequence,filename):
 			for i in range(len(char_sequence)):
 				if word[0] == char_sequence[i]:
 					possibilities[i].append(word)
-		return possibilities	
+		return possibilities
+
+def char2vec(char_sequence):
+	"""
+	para: char_sequence - a sequence of characters
+	This function takes a char_sequence and encodes it as a series of onehot vectors
+	>>> char2vec("a")
+	[[....0,0,1,0,0,0,0,0,0,0,0,...]]
+	"""
+	vector = [] 
+	for c in char_sequence:
+		char_vec = [0]*128
+		char_vec[ord(c)] = 1
+		vector.append(char_vec)
+	return vector
+
+def keras_model():
+	"""
+	Returns a keras model which we can try to fit.
+	"""
+	model = Sequential()
+	# Get rid of sparse characters. Null, etc. unlikely to be part of our model
+	model.add(Bidirectional(LSTM(70, input_dim=128, input_length=1, return_sequences=True)))
+	model.add(Bidirectional(LSTM(90, return_sequences=True)))
+	model.add(Bidirectional(LSTM(90, return_sequences=True)))
+	model.add(Bidirectional(LSTM(200, return_sequences=True)))
+	return model
+
+	
