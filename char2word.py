79d15469b9010eb2803ae5a678e19c0ebbb1430e
Arjo Chakravarty <arjo129@gmail.com>
2017-05-18 17:03:56 +0800

    trying cosine proximity

--- char2word.py
+++ char2word.py
@@ -11,7 +11,9 @@ import spacy
 import numpy as np
 import math
 import ParseJSON
+import keras.callbacks as cb
 import keras.preprocessing.sequence as sq
+from keras.models import load_model
 def char2words(char_sequence,filename="american-english.txt"):
 	"""
 	A quick and dirty example of how to enumerate all 
@@ -68,7 +70,7 @@ def keras_model():
 	model.add(Bidirectional(LSTM(100, return_sequences=True)))
 	model.add(Bidirectional(LSTM(200, return_sequences=True)))
 	model.add(Bidirectional(LSTM(150, return_sequences=True)))
-	model.compile(loss='mean_squared_error', optimizer='adam')
+	model.compile(loss='cosine_proximity', optimizer='adam')
 	return model
 
 def match_model_to_words(spacy, keystrokes, vectors):
@@ -79,14 +81,20 @@ def match_model_to_words(spacy, keystrokes, vectors):
 	possible_words = char2words(keystrokes)
 	pred = []
 	for i in range(len(vectors)):
-		min_num = math.inf
+		min_num = 0
 		min_word = ""
+		word_preds = []
 		for word in possible_words[i]:
-			dist = (spacy(word).vector - vectors[i])**2
-			s = sum(dist)
-			if min_num > s:
-				min_num = s
-				min_word = word
+			dv = spacy(word).vector
+			try:
+				dist = np.dot(dv,vectors[i])/(np.linalg.norm(dv)*np.linalg.norm(vectors[i]))
+			except:
+				pass
+			else:
+				if min_num < dist:
+					min_num = dist
+					min_word = word
+					word_preds.append(word)
 		pred.append(min_word)
 	return pred	
 
@@ -112,7 +120,7 @@ def test_training_proc():
 	print(outcome)
 	return match_model_to_words(nlp, key_strokes, outcome[0]) 
 
-def train_against_file(model, filename):
+def train_against_file(model, filename, start=0):
 	"""
 	Actual training code
 	TODO: split the files even smaller otherwise memory error raised
@@ -122,21 +130,27 @@ def train_against_file(model, filename):
 	keys, labels = list(zip(*my_file.parse_json().items()))
 	char_vecs = list(map(char2vec, keys))
 	labels = list(map(lambda x: wordseq2vec(x,nlp), labels))
-	for i in range(0,len(char_vecs),50):
+	tcb = cb.TensorBoard(log_dir='./logs')
+	for i in range(start,len(char_vecs),50):
 		if len(char_vecs) - i < 50: 
 			print("last round")
 			X = sq.pad_sequences(char_vecs[i:], maxlen=100)
 			Y = sq.pad_sequences(labels[i:], maxlen=100)
-			model.fit(X, Y, epochs=100)
+			model.fit(X, Y, epochs=100, callbacks=[tcb])
 		else:
-			print("data", 100*i/(len(char_vecs))//50)
+			print("data", 100*i/(len(char_vecs)/50))
 			X = sq.pad_sequences(char_vecs[i:i+100], maxlen=100)
 			Y = sq.pad_sequences(labels[i:i+100], maxlen=100)
-			model.fit(X, Y, epochs=100)
+			model.fit(X, Y, epochs=20,  batch_size=50, callbacks=[tcb])
+		print("backing up")
+		model.save("model.h5")
 	return match_model_to_words(nlp,"Ilef",model.predict(np.array([char2vec("Ilef")]))[0])
- 
+
+def predictor(nlp, model, keystrokes):
+	return match_model_to_words(nlp,keystrokes,model.predict(np.array([char2vec(keystrokes)]))[0]) 
 #print(test_matching_function())
 #print(test_training_proc())
 #print(test_word2vec())
 #print(train_against_file(keras_model(),"example_training_data.json"))
-print(train_against_file(keras_model(), "/media/arjo/EXT4ISAWESOME/tmlc1-training-01/tmlc1-training-001.json"))
+if __name__ == "__main__":
+	train_against_file(keras_model(), "/media/arjo/EXT4ISAWESOME/tmlc1-training-01/tmlc1-training-001.json", start=750)
